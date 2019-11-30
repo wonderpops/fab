559fc4c5a63d33a2292224d643bd780f26fd3055
wonderpops <supstump@gmail.com>
2019-11-30 17:23:53 +1000

    Add navbar in index.php

--- index.php
+++ index.php
@@ -15,12 +15,78 @@
         <title>Главная страница</title>
     </head>
     <body>
-        <?php if ($login === null): ?>
-            <a href="/login.php">Авторизуйтесь</a>
-        <?php else: ?>
-            Добро пожаловать, <?= $login ?>
-            <br>
-            <a href="/logout.php">Выйти</a>
-        <?php endif; ?>
+
+    <!-- NAVBAR -->
+    <nav class="navbar is-light" role="navigation" aria-label="main navigation">
+        <div class="navbar-brand">
+            <a class="navbar-item" href="https://bulma.io">
+                <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
+            </a>
+
+            <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+            </a>
+        </div>
+
+        <div id="navbarBasicExample" class="navbar-menu">
+            <div class="navbar-start">
+                <a class="navbar-item">
+                    Главная
+                </a>
+
+                <a class="navbar-item">
+                    Машины
+                </a>
+
+                <a class="navbar-item">
+                    Поиск
+                </a>
+
+                <div class="navbar-item has-dropdown is-hoverable">
+                    <a class="navbar-link">
+                        Детали
+                    </a>
+
+                    <div class="navbar-dropdown">
+                        <a class="navbar-item">
+                            На складе
+                        </a>
+                        <a class="navbar-item">
+                            Проданные
+                        </a>
+                        <a class="navbar-item">
+                            Все
+                        </a>
+                        <hr class="navbar-divider">
+                        <a class="navbar-item">
+                            Report an issue
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="navbar-end">
+                <div class="navbar-item">
+                    <div class="buttons">
+                        <a class="button is-link">
+                            <span class="icon is-small">
+                                <i class="fas fa-user"></i>
+                            </span>
+                        <strong><?php ?>Добро пожаловать, <?= $login ?></strong>
+                        </a>
+                        <a href="/logout.php" class="button is-link is-inverted">
+                            <span class="icon is-small">
+                                <i class="fas fa-sign-out-alt"></i>
+                            </span>
+                            <strong>Выйти</strong>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
+
     </body>
 </html>
