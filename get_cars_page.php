73f62ff9e4d8cb3953316761bab63a484555e95b
wonderpops <supstump@gmail.com>
2020-02-19 16:03:10 +1100

    Add cars page

---
+++ get_cars_page.php
@@ -0,0 +1,87 @@
+<?php
+include 'db_connect.php';
+
+$name = $_GET['frequest'];
+
+$sql = "SELECT * FROM `cars`";
+$result = mysqli_query($conn, $sql);
+
+echo('<div class="column is-2">
+<nav class="panel panel_search">
+    <p class="panel-heading">Поиск</p>
+    <div class="panel-block">
+        <p class="control has-icons-left">
+        <input class="input" type="text" placeholder="Search">
+        <span class="icon is-left">
+            <i class="fas fa-search" aria-hidden="true"></i>
+        </span>
+        </p>
+    </div>
+    <p class="panel-tabs">
+        <a class="is-active">Все</a>
+        <a>Разбираются</a>
+        <a>Разобранные</a>
+    </p>
+    <a class="panel-block is-active">
+        <span class="panel-icon">
+        <i class="fas fa-book" aria-hidden="true"></i>
+        </span>
+        bulma
+    </a>
+    <label class="panel-block">
+        <input type="checkbox">
+        remember me
+    </label>
+    <div class="panel-block">
+        <button class="button is-link is-outlined is-fullwidth">
+        Reset all filters
+        </button>
+    </div>
+</nav>
+</div>');
+
+echo('<div class="column is-8">
+        <div id = "main_contant" class = "main-contant">
+            <div id="results" class="columns is-multiline">');
+
+while ($row = mysqli_fetch_assoc($result)) {
+    echo('<div class="column is-half">');
+    echo('<div class="card ">
+        <div class="card-image">');
+        echo ('<img src="res/alloy-wheel.png"');
+        echo('</img>
+        </div>
+        <div class="card-content">
+            <div class="media">
+                <div class="media-content">
+                    <p class="title is-4">');
+                    echo $row['name'];
+                    echo('</p>
+                    <p class="subtitle is-6">');
+                    echo $row['comment'];
+                    echo('</p>
+                </div>
+            </div>
+        </div>
+        <footer class="card-footer">
+            <a href="?page=car&id=');
+            echo $row['id'];
+            echo('" class="card-footer-item">Изменить</a>
+        </footer>
+    </div>');
+    echo('</div>');
+}
+echo('</div></div></div>');
+
+echo('<div class="column is-2">
+<nav class="panel panel_search">
+    <p class="panel-heading">Редактировать</p>
+    <div class="panel-block">
+        <a class="button is-link is-outlined is-fullwidth" href="?page=add_car">Добавить</a>
+    </div>
+    <div class="panel-block">
+        <button class="button is-link is-outlined is-fullwidth">Удалить</button>
+    </div>
+</nav>
+</div>');
+?>
--- index.php
+++ index.php
@@ -9,6 +9,7 @@
     <head>
     <meta charset="UTF-8">
     <script src="lib/anime.min.js"></script>
+    <script src="http://code.jquery.com/jquery-latest.js"></script>
     <link rel="stylesheet" href="styles/style.css" type="text/css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.8.0/css/bulma.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -17,83 +18,103 @@
     </head>
     <body>
 
-    <!-- LOADING -->
-    <div class="loading">
-        <span class = "wheel"></span>
-        <script src="js/loading.js"></script>
-    </div>
-
-    <!-- NAVBAR -->
-    <nav class="navbar is-light" role="navigation" aria-label="main navigation">
-        <div class="navbar-brand">
-            <a class="navbar-item" href="https://bulma.io">
-                <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
-            </a>
-
-            <a role="button" onclick="document.querySelector('#navMenu').classList.toggle('is-active');this.classList.toggle('is-active');" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-            </a>
+        <!-- LOADING -->
+        <div class="loading">
+            <span class = "wheel"></span>
+            <script src="js/loading.js"></script>
         </div>
 
-        <div id="navMenu" class="navbar-menu">
-            <div class="navbar-start">
-                <a class="navbar-item">
-                    Главная
+        <!-- NAVBAR -->
+        <nav class="navbar is-light" role="navigation" aria-label="main navigation">
+            <div class="navbar-brand">
+                <a class="navbar-item" href="https://bulma.io">
+                    <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
                 </a>
 
-                <a class="navbar-item">
-                    Машины
+                <a role="button" onclick="document.querySelector('#navMenu').classList.toggle('is-active');this.classList.toggle('is-active');" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
                 </a>
+            </div>
 
-                <a class="navbar-item">
-                    Поиск
-                </a>
-
-                <div class="navbar-item has-dropdown is-hoverable">
-                    <a class="navbar-link">
-                        Детали
+            <div id="navMenu" class="navbar-menu">
+                <div class="navbar-start">
+                    <a class="navbar-item">
+                        Главная
                     </a>
 
-                    <div class="navbar-dropdown">
-                        <a class="navbar-item">
-                            На складе
-                        </a>
-                        <a class="navbar-item">
-                            Проданные
-                        </a>
-                        <a class="navbar-item">
-                            Все
-                        </a>
-                        <hr class="navbar-divider">
-                        <a class="navbar-item">
-                            Report an issue
-                        </a>
-                    </div>
-                </div>
-            </div>
+                    <a class="navbar-item" href='?page=cars'>
+                        Машины
+                    </a>
 
-            <div class="navbar-end">
-                <div class="navbar-item">
-                    <div class="buttons">
-                        <a class="button is-link is-rounded">
-                            <span class="icon is-small">
-                                <i class="fas fa-user"></i>
-                            </span>
-                        <strong><?php ?>Добро пожаловать, <?= $login ?></strong>
+                    <a class="navbar-item">
+                        Поиск
+                    </a>
+
+                    <div class="navbar-item has-dropdown is-hoverable">
+                        <a class="navbar-link">
+                            Детали
                         </a>
-                        <a href="/logout.php" class="button is-link is-inverted is-rounded">
-                            <span class="icon is-small">
-                                <i class="fas fa-sign-out-alt"></i>
-                            </span>
-                            <strong>Выйти</strong>
-                        </a>
+
+                        <div class="navbar-dropdown">
+                            <a class="navbar-item">
+                                На складе
+                            </a>
+                            <a class="navbar-item">
+                                Проданные
+                            </a>
+                            <a class="navbar-item">
+                                Все
+                            </a>
+                            <hr class="navbar-divider">
+                            <a class="navbar-item">
+                                Report an issue
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="navbar-end">
+                    <div class="navbar-item">
+                        <div class="buttons">
+                            <a class="button is-link is-rounded">
+                                <span class="icon is-small">
+                                    <i class="fas fa-user"></i>
+                                </span>
+                            <strong><?php ?>Добро пожаловать, <?= $login ?></strong>
+                            </a>
+                            <a href="/logout.php" class="button is-link is-inverted is-rounded">
+                                <span class="icon is-small">
+                                    <i class="fas fa-sign-out-alt"></i>
+                                </span>
+                                <strong>Выйти</strong>
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
+        </nav>
+
+        <!-- main-container -->
+        <div id="main_container" class="columns main-container">
+            
         </div>
-    </nav>
 
     </body>
+    <script src="js/page_changer.js"></script>
+    <?php
+        switch ($_GET['page']){
+            case "cars":
+                echo('<script>getPage("cars");</script>');
+                break;
+            case "add_car":
+                echo('<script>getPage("add_car");</script>');
+                break;
+            case "car":
+                $id = $_GET['id'];
+                echo "<script>getPage('car', $id);</script>')";
+                break;
+        }
+    ?>
 </html>
