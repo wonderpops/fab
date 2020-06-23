bd97f7a0b44076f4d9be28e2ae2d714cb9331fea
root <root@wonderpop.dev>
2020-06-24 03:23:47 +1000

    Bug fixes in controllers

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -8,6 +8,10 @@ class ImageController extends Controller
 {
     public function upload(Request $request){
         $path = $request->file('image')->store('uploads', 'public');
-        return view('cars.add', compact('path'));
+        return redirect()->back()->with('path', $path);
+        //return redirect()->route($request -> last_route, ['path' => $path]);
+        //return redirect(url()->previous())->with('path', $path);
+        // return view($request-> view_name, compact('path'));
     }
 }
+?>
--- app/Http/Controllers/PartController.php
+++ app/Http/Controllers/PartController.php
@@ -14,6 +14,48 @@ class PartController extends Controller
 
     public function show($id){
         $part = part::find($id);
-        return view('part.show', compact('part'));
+        return view('parts.show', compact('part'));
+    }
+
+    public function add(){
+        return view('parts.add');
+    }
+
+    public function save_changes(Request $request){
+        $validator = $request->validate([
+            'name' => 'required|max:255',
+            'type' => 'required|max:255',
+            'cost' => 'required|max:255'
+        ]);
+
+        $part = Part::find($request -> part_id);
+        $part->name = $request -> name;
+        $part->type = $request -> type;
+        $part->cost = $request -> cost;
+        $part->status = $request -> status;
+        //$part->status = 'Разбирается';
+        $part->save();
+        return redirect('/parts/'.$request -> part_id);
+    }
+
+    public function add_new_part(Request $request){
+        $validator = $request->validate([
+            'name' => 'required|max:255',
+            'type' => 'required|max:255',
+            'status' => 'required|max:255',
+            'car_id' => 'required|max:255',
+            'image' => 'required',
+            'cost' => 'required|max:255'
+        ]);
+
+        $part = new Part;
+        $part->name = $request -> name;
+        $part->type = $request -> type;
+        $part->cost = $request -> cost;
+        $part->status = $request -> status;
+        $part->car_id = $request -> car_id;
+        $part->image = $request -> image;
+        $part->save();
+        return redirect('/parts');
     }
 }
